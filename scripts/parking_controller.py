1f1628b1b26457dfaa441dca8954f5ce1a1651b1
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2023-03-16 21:37:10 -0400

    Tuning params

--- scripts/parking_controller.py
+++ scripts/parking_controller.py
@@ -67,7 +67,7 @@ class ParkingController():
         turn_angle = math.atan(L / R)
         drive_speed = 1.0
         at_correct_distance = abs(L_1  - self.parking_distance) < 0.12
-        correct_orientation = abs(theta) < math.radians(25.0)
+        correct_orientation = abs(theta) < math.radians(7.5)
         now = rospy.Time.now().to_sec()
         if self.reverse:
             if now - self.time_start_reverse >= self.REVERSE_TIME_SEC:
