e96795902ae3690a33b4441d73005a1bd5f1b40c
Ayyub Abdulrezak <ayyubabdul01@gmail.com>
2023-03-16 21:02:49 -0400

    Tuning params

--- scripts/parking_controller.py
+++ scripts/parking_controller.py
@@ -61,7 +61,7 @@ class ParkingController():
         L = self.CAR_LENGTH
         R = L_1 / (2 * math.sin(theta))
 
-        # print(theta)
+        print(theta, math.radians(25.0))
 
         turn_angle = math.atan(L / R)
         drive_speed = 1.0
